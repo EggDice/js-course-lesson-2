6eeab7044243dd4fdcb72addf58ed4f66c19be3c
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2014-10-29 16:36:28 +0100

    solution

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 'use strict';
 
-function potter() {
+var _ = require('lodash');
 
+var DISCOUNTS = {
+  '1': 1,
+  '2': 0.95,
+  '3': 0.90,
+  '4': 0.80,
+  '5': 0.75
+};
+
+var BASE_PRICE = 8;
+
+function potter(books) {
+  return getCombos(books).reduce(function(price, combo) {
+    return price + getComboPriceByLength(combo.length);
+  }, 0);
+}
+
+function getCombos(books) {
+  return books.reduce(pushCombos, []);
+}
+
+function pushCombos(combos, book) {
+  var combo = getBestAppendPriceCombo(book, combos);
+  if (!combo.length) {
+    combos.push(combo);
+  }
+  combo.push(book);
+  return combos;
+}
+
+function getBestAppendPriceCombo(book, combos) {
+  return combos.filter(_.curry(isBookInCombo)(book)).
+    reduce(getBetterAppendPriceCombo, []);
+}
+
+function getComboPriceByLength(length) {
+  return length * BASE_PRICE * DISCOUNTS[length];
+}
+
+function isBookInCombo(book, combo) {
+  return combo.indexOf(book) === -1;
+}
+
+function getBetterAppendPriceCombo(combo1, combo2) {
+  return getAppendPrice(combo1) > getAppendPrice(combo2) ? combo1 : combo2;
+}
+
+function getAppendPrice(combo) {
+  var l = combo.length;
+  return getComboPriceByLength(l) - getComboPriceByLength(l + 1);
 }
 
 module.exports = potter;
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,5 +6,25 @@ var potter = require('../');
 test('potter', function(t) {
   t.equal(typeof potter, 'function', 'it should be a function');
 
+  t.equal(potter([]), 0);
+  t.equal(potter([1]), 8);
+  t.equal(potter([2]), 8);
+  t.equal(potter([3]), 8);
+  t.equal(potter([4]), 8);
+  t.equal(potter([5]), 8);
+
+  t.equal(potter([1, 1]), 2 * 8);
+
+  t.equal(potter([1, 2]), 2 * 8 * 0.95);
+  t.equal(potter([1, 2, 3]), 3 * 8 * 0.90);
+  t.equal(potter([1, 2, 3, 4]), 4 * 8 * 0.80);
+  t.equal(potter([1, 2, 3, 4, 5]), 5 * 8 * 0.75);
+
+  t.equal(potter([1, 1, 2]), 2 * 8 * 0.95 + 8);
+  t.equal(potter([1, 1, 2, 2]), 4 * 8 * 0.95);
+  t.equal(potter([1, 2, 2, 3, 4, 5]), 5 * 8 * 0.75 + 8);
+  t.equal(potter([1, 1, 1, 2, 2, 2]), 6 * 8 * 0.95);
+  t.equal(potter([1, 1, 2, 2, 3, 3, 4, 5]), 2 * 4* 8 * 0.80);
+
   t.end();
 });
